c82a45871eb57c8b12d2e533f62c64f155e266f7
Jun Yan <jun.yan@uconn.edu>
2022-11-07 13:51:08 -0500

    experiment with bootstrap preserving serial correlation

--- Code/babu.R
+++ Code/babu.R
@@ -41,27 +41,3 @@ ks.np.babu <- function(x, B = 1000) {
     list(p.value = p.value,
          statistic = stat, stat.b = stat.b)
 }
-    
-## test for effective sample size
-ks.eff <- function(x) {
-    Fn <- ecdf(x)
-    Gn <- ecdf.leftcont(x)
-    mu <- mean(x); ss <- sd(x)
-    Ftheta <- pnorm(x, mu, ss)
-    Bn1 <- Fn(x) - Ftheta
-    Bn2 <- Gn(x) - Ftheta
-    d1 <- abs(Bn1)
-    d2 <- abs(Bn2)
-    stat <- max(d1, d2)
-    n <- coda::effectiveSize(x)
-    ## two-sided
-    pval <- 1 - .Call(stats:::C_pKS2, sqrt(n) * stat, tol = 1e-06)
-    list(p.value = pval, statistic = stat, neff = n)
-}
-
-do1rep <- function(phi = 0.5, n = 1000) {
-    x <- arima.sim(model = list(ar = phi), n = n, sd = sqrt(1 - phi^2))
-    ks.eff(x)$p.value
-}
-
-
