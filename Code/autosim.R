69a9502e920ca795cf905e805d066398ed32b28e
Jun Yan <jun.yan@uconn.edu>
2022-12-05 13:52:21 -0500

    auto.arima with no intercept in fitting

---
+++ Code/autosim.R
@@ -0,0 +1,17 @@
+source("autoarma.R")
+
+## arma(1, 1) with phi = .3 and theta = .2
+sim.3.2 <- replicate(1000, do1rep(.3, .2, 200, pdist, qdist, "1,1"))
+sim.3.2a <- replicate(1000, do1rep(.3, .2, 200, pdist, qdist, "auto"))
+
+## ar(1) with phi = .5
+sim.5 <- replicate(500, do1rep(.5, double(0), 200, pdist, qdist, "1,0"))
+sim.5a <- replicate(500, do1rep(.5, double(0), 200, pdist, qdist, "auto"))
+
+
+## ar(1) with phi = -.3
+sim.n3 <- replicate(1000, do1rep(-.3, double(0), 200, pdist, qdist, "1,0"))
+sim.n3a <- replicate(1000, do1rep(.3, double(0), 200, pdist, qdist, "auto"))
+
+
+save(sim.3.2, sim.3.2a sim.5, sim.5a, sim.n3, sim.n3a, file = "autosim.rda")
