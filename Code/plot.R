2e5343cf810f1a35aa8609301d1ca9a217f35e31
Jun Yan <jun.yan@uconn.edu>
2023-04-10 21:34:30 -0400

    added pp-plots to manuscript

--- Code/plot.R
+++ Code/plot.R
@@ -4,6 +4,7 @@ load("sim.rda")
 
 ## Section 2: Fitted parameters
 gg.f <- ggplot(data = f.data, mapping = aes(sample = p)) +
+    scale_x_continuous(breaks=c(0, 0.5, 1.0)) + 
     stat_pp_band(distribution = "unif") +
     stat_pp_line() +
     stat_pp_point(distribution = "unif", cex = .3) +
@@ -16,6 +17,7 @@ ggsave(filename = "pp_fit.pdf", plot = gg.f, path = "../Manuscript", height = 4,
 
 ## Section 3: Demonstrate the consequence of serial dependence
 gg.s <- ggplot(data = s.data, mapping = aes(sample = p)) +
+    scale_x_continuous(breaks=c(0, 0.5, 1.0)) +
     stat_pp_band(distribution = "unif") +
     stat_pp_line() +
     stat_pp_point(distribution = "unif", cex = .3) +
@@ -23,11 +25,12 @@ gg.s <- ggplot(data = s.data, mapping = aes(sample = p)) +
     labs(x = "Probability Points", y = "Cumulative Probability") +
     coord_fixed() # theme(aspect.ratio=1)
 
-ggsave(filename = "pp_s.pdf", plot = gg.s, path = "../Manuscript", height = 3, width = 6)
+ggsave(filename = "pp_s.pdf", plot = gg.s, path = "../Manuscript", height = 3.5, width = 6)
 
 ## corrected for serial dependence
 gg.ss <- ggplot(data = subset(ss.data, meth == "bootstrap"),
              mapping = aes(sample = p)) +
+    scale_x_continuous(breaks=c(0, 0.5, 1.0)) +
     stat_pp_band(distribution = "unif") +
     stat_pp_line() +
     stat_pp_point(distribution = "unif", cex = .3) +
@@ -35,13 +38,13 @@ gg.ss <- ggplot(data = subset(ss.data, meth == "bootstrap"),
     labs(x = "Probability Points", y = "Cumulative Probability") +
     coord_fixed() # theme(aspect.ratio=1)
 
-ggsave(filename = "pp_ss.pdf", plot = gg.ss, path = "../Manuscript", height = 3, width = 6)
+ggsave(filename = "pp_ss.pdf", plot = gg.ss, path = "../Manuscript", height = 3, width = 4.5)
 
 ## Section 4: fitted parameter and serial dependence
 
-ssf.data$meth <- gl(3, nrep, nrep * 18, c("naive0", "naive", "bootstrap"))
 gg.ssf <- ggplot(data = subset(ssf.data, meth == "bootstrap"),
                  mapping = aes(sample = p)) +
+    scale_x_continuous(breaks=c(0, 0.5, 1.0)) +
     stat_pp_band(distribution = "unif") +
     stat_pp_line() +
     stat_pp_point(distribution = "unif", cex = .3) +
@@ -49,19 +52,23 @@ gg.ssf <- ggplot(data = subset(ssf.data, meth == "bootstrap"),
     labs(x = "Probability Points", y = "Cumulative Probability") +
     coord_fixed() # theme(aspect.ratio=1)
 
-ggsave(filename = "pp_ssf.pdf", plot = gg.ssf, path = "../Manuscript", height = 3, width = 6)
+ggsave(filename = "pp_ssf.pdf", plot = gg.ssf, path = "../Manuscript", height = 3, width = 4.5)
 
 gg.s_f <- ggplot(data = subset(ssf.data, meth == "naive"),
                  mapping = aes(sample = p)) +
+    scale_x_continuous(breaks=c(0, 0.5, 1.0)) +
     stat_pp_band(distribution = "unif") +
     stat_pp_line() +
     stat_pp_point(distribution = "unif", cex = .3) +
     facet_grid(vars(dist), vars(dep)) +
     labs(x = "Probability Points", y = "Cumulative Probability") +
     coord_fixed() # theme(aspect.ratio=1)
+
+ggsave(filename = "pp_s_f.pdf", plot = gg.s_f, path = "../Manuscript", height = 3, width = 4.5)
 
 gg.s__ <- ggplot(data = subset(ssf.data, meth == "naive0"),
                  mapping = aes(sample = p)) +
+    scale_x_continuous(breaks=c(0, 0.5, 1.0)) +
     stat_pp_band(distribution = "unif") +
     stat_pp_line() +
     stat_pp_point(distribution = "unif", cex = .3) +
@@ -69,4 +76,4 @@ gg.s__ <- ggplot(data = subset(ssf.data, meth == "naive0"),
     labs(x = "Probability Points", y = "Cumulative Probability") +
     coord_fixed() # theme(aspect.ratio=1)
 
-ggsave(filename = "pp_s_.pdf", plot = gg.s__, path = "../Manuscript", height = 3, width = 6)
+ggsave(filename = "pp_s_.pdf", plot = gg.s__, path = "../Manuscript", height = 3, width = 4.5)
