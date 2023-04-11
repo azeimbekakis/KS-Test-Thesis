9677b0c32015cb1229f1437df48a8b53fb993077
Jun Yan <jun.yan@uconn.edu>
2023-04-11 06:55:45 -0400

    sparser ticks in pp-plots

--- Code/plot.R
+++ Code/plot.R
@@ -4,7 +4,8 @@ load("sim.rda")
 
 ## Section 2: Fitted parameters
 gg.f <- ggplot(data = f.data, mapping = aes(sample = p)) +
-    scale_x_continuous(breaks=c(0, 0.5, 1.0)) + 
+    scale_x_continuous(breaks=c(0, 1)) +
+    scale_y_continuous(breaks=c(0, 1)) + 
     stat_pp_band(distribution = "unif") +
     stat_pp_line() +
     stat_pp_point(distribution = "unif", cex = .3) +
@@ -12,12 +13,13 @@ gg.f <- ggplot(data = f.data, mapping = aes(sample = p)) +
     labs(x = "Probability Points", y = "Cumulative Probability") +
     coord_fixed() # theme(aspect.ratio=1)
 
-ggsave(filename = "pp_fit.pdf", plot = gg.f, path = "../Manuscript", height = 4, width = 4)
+ggsave(filename = "pp_f.pdf", plot = gg.f, path = "../Manuscript", height = 4, width = 4)
 
 
 ## Section 3: Demonstrate the consequence of serial dependence
 gg.s <- ggplot(data = s.data, mapping = aes(sample = p)) +
-    scale_x_continuous(breaks=c(0, 0.5, 1.0)) +
+    scale_x_continuous(breaks=c(0, 1)) +
+    scale_y_continuous(breaks=c(0, 1)) + 
     stat_pp_band(distribution = "unif") +
     stat_pp_line() +
     stat_pp_point(distribution = "unif", cex = .3) +
@@ -30,7 +32,8 @@ ggsave(filename = "pp_s.pdf", plot = gg.s, path = "../Manuscript", height = 3.5,
 ## corrected for serial dependence
 gg.ss <- ggplot(data = subset(ss.data, meth == "bootstrap"),
              mapping = aes(sample = p)) +
-    scale_x_continuous(breaks=c(0, 0.5, 1.0)) +
+    scale_x_continuous(breaks=c(0, 1)) +
+    scale_y_continuous(breaks=c(0, 1)) + 
     stat_pp_band(distribution = "unif") +
     stat_pp_line() +
     stat_pp_point(distribution = "unif", cex = .3) +
@@ -44,7 +47,8 @@ ggsave(filename = "pp_ss.pdf", plot = gg.ss, path = "../Manuscript", height = 3,
 
 gg.ssf <- ggplot(data = subset(ssf.data, meth == "bootstrap"),
                  mapping = aes(sample = p)) +
-    scale_x_continuous(breaks=c(0, 0.5, 1.0)) +
+    scale_x_continuous(breaks=c(0, 1)) +
+    scale_y_continuous(breaks=c(0, 1)) + 
     stat_pp_band(distribution = "unif") +
     stat_pp_line() +
     stat_pp_point(distribution = "unif", cex = .3) +
@@ -56,7 +60,8 @@ ggsave(filename = "pp_ssf.pdf", plot = gg.ssf, path = "../Manuscript", height = 
 
 gg.s_f <- ggplot(data = subset(ssf.data, meth == "naive"),
                  mapping = aes(sample = p)) +
-    scale_x_continuous(breaks=c(0, 0.5, 1.0)) +
+    scale_x_continuous(breaks=c(0, 1)) +
+    scale_y_continuous(breaks=c(0, 1)) + 
     stat_pp_band(distribution = "unif") +
     stat_pp_line() +
     stat_pp_point(distribution = "unif", cex = .3) +
@@ -68,7 +73,8 @@ ggsave(filename = "pp_s_f.pdf", plot = gg.s_f, path = "../Manuscript", height = 
 
 gg.s__ <- ggplot(data = subset(ssf.data, meth == "naive0"),
                  mapping = aes(sample = p)) +
-    scale_x_continuous(breaks=c(0, 0.5, 1.0)) +
+    scale_x_continuous(breaks=c(0, 1)) +
+    scale_y_continuous(breaks=c(0, 1)) + 
     stat_pp_band(distribution = "unif") +
     stat_pp_line() +
     stat_pp_point(distribution = "unif", cex = .3) +
